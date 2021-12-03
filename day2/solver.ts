1badcac8e740272c0d11873a463f6b4f4ca7de4e
Abilio Almeida Eiroa <bili@nextail.co>
2021-12-03 19:09:42 +0100

    Add day2's part 2 solution

--- day2/solver.ts
+++ day2/solver.ts
@@ -7,10 +7,15 @@ export function part1(input: string[]): number {
 }
 
 export function part2(input: string[]): number {
-  return 900;
+  const commands = commandsFromInputsLines(input);
+  const submarine = new AdvancedSubmarine();
+  submarine.run(commands);
+
+  return submarine.x * submarine.y;
 }
 
 type CommandDirection = "forward" | "down" | "up";
+
 interface ICommand {
   direction: CommandDirection;
   distance: number;
@@ -20,6 +25,10 @@ interface ISubmarine {
   x: number;
   y: number;
   run: (commands: ICommand[]) => void;
+}
+
+interface IAdvancedSubmarine extends ISubmarine {
+  aim: number;
 }
 
 function commandsFromInputsLines(input: string[]): ICommand[] {
@@ -58,5 +67,30 @@ class Submarine extends AbstractSubamarine {
 
   up(distance: number) {
     this.x -= distance;
+  }
+}
+
+class AdvancedSubmarine
+  extends AbstractSubamarine
+  implements IAdvancedSubmarine
+{
+  aim: number;
+
+  constructor() {
+    super();
+    this.aim = 0;
+  }
+
+  forward(distance: number) {
+    this.x += distance;
+    this.y += this.aim * distance;
+  }
+
+  down(distance: number) {
+    this.aim += distance;
+  }
+
+  up(distance: number) {
+    this.aim -= distance;
   }
 }
--- day2/test.ts
+++ day2/test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
 
-import { part1 } from "./solver.ts";
+import { part1, part2 } from "./solver.ts";
 
 Deno.test("Part 1", () => {
   const input = [
@@ -9,10 +9,25 @@ Deno.test("Part 1", () => {
     "forward 8",
     "up 3",
     "down 8",
-    "forward 2"
+    "forward 2",
   ];
   const actual = part1(input);
   const expected = 150;
+
+  assertEquals(actual, expected);
+});
+
+Deno.test("Part 2", () => {
+  const input = [
+    "forward 5",
+    "down 5",
+    "forward 8",
+    "up 3",
+    "down 8",
+    "forward 2",
+  ];
+  const actual = part2(input);
+  const expected = 900;
 
   assertEquals(actual, expected);
 });
